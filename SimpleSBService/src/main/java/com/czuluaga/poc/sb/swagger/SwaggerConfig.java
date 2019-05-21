738cadd5d15949293be0f5c7c22d73de211cdb69
carlos <carlos.zuluaga@gmail.com>
2019-05-20 21:26:46 -0500

    minor modifications

--- SimpleSBService/src/main/java/com/czuluaga/poc/sb/swagger/SwaggerConfig.java
+++ SimpleSBService/src/main/java/com/czuluaga/poc/sb/swagger/SwaggerConfig.java
@@ -1,5 +1,8 @@
 package com.czuluaga.poc.sb.swagger;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,18 +18,25 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+	
+	private Set<String> setProduceer(){
+		HashSet<String> PRODUCES = new HashSet<String>();
+		PRODUCES.add("application/json");
+		return PRODUCES;
+	}
 
+	
 	@Bean
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2).select()
 				.apis(RequestHandlerSelectors.basePackage("com.czuluaga.poc.sb.rs"))
-				.paths(PathSelectors.regex("/.*")).build().apiInfo(apiEndPointsInfo());
+				.paths(PathSelectors.regex("/.*")).build().apiInfo(apiEndPointsInfo()).produces(setProduceer());
 	}
+	
 
 	private ApiInfo apiEndPointsInfo() {
 		return new ApiInfoBuilder().title("Spring Boot REST API")
-								   .description("Employee Management REST API")
-								   //.contact(new Contact("Ramesh Fadatare", "www.javaguides.net", "ramesh24fadatare@gmail.com"))
+								   .description("Library Management REST API")
 								   .contact(new Contact("Carlos Zuluaga", "https://github.com/czuluagah", "carlos.zuluaga@gmail.com"))
 								   .license("Apache 2.0")
 								   .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html").version("1.0.0")
